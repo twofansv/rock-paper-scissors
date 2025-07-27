5b84dbd4d3347a69c81aa4b43321a7fbc1b88989
twofansv <95734976+twofansv@users.noreply.github.com>
2025-07-27 21:09:25 +0800

    Added function for generating the choice as string for RPS.

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
+// MAKE A NUMBER GENERATOR FROM NUMBER 0 TO 3
+// BOX FOR NUMBER GENERATED
+// IF THE GENERATED NUMBER IS EQUALS TO 1 THEN IT MUST PLAY "ROCK" ELSE IF .. UPTO SCISSORS ..
+///  
+
+
+
+
+
+
+
+
+
+
+//RANDOM NUMBER GENERATOR
 function getComputerChoice(){
-    let numberChoice = Math.random() * 3;
+    let numberChoice = Math.floor(Math.random() * 3);
     return numberChoice;
 }
 
+let numberInstance = getComputerChoice();
+
+function forTest(){
+    
+    
+if (numberInstance == 0) {
+    console.log("Rock")
+}
+else if (numberInstance == 1) {
+    console.log("Paper")
+}
+else if (numberInstance == 2) {
+    console.log("Scissors")
+    
+}
+return numberInstance;
+}
