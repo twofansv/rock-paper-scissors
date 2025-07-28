eee898f1b45b62dba594dd726806c45f8c526f00
twofansv <95734976+twofansv@users.noreply.github.com>
2025-07-28 22:36:41 +0800

    Added function for getting human choice and added WIP compare computer choice vs. human choice function.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // MAKE A NUMBER GENERATOR FROM NUMBER 0 TO 3
 // BOX FOR NUMBER GENERATED
 // IF THE GENERATED NUMBER IS EQUALS TO 1 THEN IT MUST PLAY "ROCK" ELSE IF .. UPTO SCISSORS ..
-///  
+/// 
 
 
 
@@ -15,23 +15,79 @@
 //RANDOM NUMBER GENERATOR
 function getComputerChoice(){
     let numberChoice = Math.floor(Math.random() * 3);
-    return numberChoice;
-}
 
-let numberInstance = getComputerChoice();
-
-function forTest(){
-    
-    
-if (numberInstance == 0) {
+if (numberChoice == 0) {
     console.log("Rock")
 }
-else if (numberInstance == 1) {
+else if (numberChoice == 1) {
     console.log("Paper")
 }
-else if (numberInstance == 2) {
+else if (numberChoice == 2) {
     console.log("Scissors")
     
 }
-return numberInstance;
+return numberChoice;
 }
+   
+
+
+
+
+getComputerChoice();
+getHumanChoice();
+versusChoice();
+
+
+function versusChoice() {
+    if (getComputerChoice == 'Rock' && getHumanChoice == 'Paper') {
+        console.log('You Won!');
+    }
+    else if (getComputerChoice == 'Rock' && getHumanChoice == 'Scissors') {
+        console.log('You Lose!')
+    }
+    else if (getComputerChoice == 'Rock' && getHumanChoice == 'Rock') {
+        console.log('Draw!')
+    }
+}
+
+function getHumanChoice() {
+    let humanChoice = prompt('Input number for: 1. Rock 2. Paper 3. Scissors');
+    
+
+    if (humanChoice == 1) {
+        console.log('Rock');
+    }
+    else if (humanChoice == 2) {
+        console.log('Paper')
+    }
+    else if (humanChoice == 3) {
+        console.log('Scissors')
+    }
+    return humanChoice;
+    
+}
+    
+//ROCK PAPER SCISSORS AS STRING GENERATION  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
