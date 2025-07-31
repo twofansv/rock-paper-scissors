fc75d738bffac70550b64f94f319d3d67e6cfe1e
twofansv <95734976+twofansv@users.noreply.github.com>
2025-07-31 21:17:11 +0800

    Made all if statement to switches.

--- script.js
+++ script.js
@@ -15,59 +15,69 @@
 //RANDOM NUMBER GENERATOR
 function getComputerChoice(){
     let numberChoice = Math.floor(Math.random() * 3);
-
-if (numberChoice == 0) {
-    console.log("Rock")
-}
-else if (numberChoice == 1) {
-    console.log("Paper")
-}
-else if (numberChoice == 2) {
-    console.log("Scissors")
-    
-}
-return numberChoice;
+switch(numberChoice){
+    case 0:
+        return 'Rock'
+    case 1:
+        return 'Paper'
+    case 2:
+        return 'Scissors'
+    }
 }
    
 
 
 
 
-getComputerChoice();
-getHumanChoice();
+
+
 versusChoice();
 
 
 function versusChoice() {
-    if (getComputerChoice == 'Rock' && getHumanChoice == 'Paper') {
-        console.log('You Won!');
+
+    if (getComputerChoice() == 'Rock' && getHumanChoice() == 'Paper') {
+        console.log('You Won! Paper beats Rock!');
     }
-    else if (getComputerChoice == 'Rock' && getHumanChoice == 'Scissors') {
-        console.log('You Lose!')
+    else if (getComputerChoice() == 'Rock' && getHumanChoice() == 'Scissors') {
+        console.log('You Lose! Rock beats Scissors!')
     }
-    else if (getComputerChoice == 'Rock' && getHumanChoice == 'Rock') {
-        console.log('Draw!')
+    else if (getComputerChoice() == 'Rock' && getHumanChoice() == 'Rock') {
+        console.log('Draw! Both is Rock!')
     }
+    else if (getComputerChoice() == 'Paper' && getHumanChoice() == 'Scissors') {
+        console.log('You Won! Scissors beats Paper!')
+    }
+    else if (getComputerChoice() == 'Paper' && getHumanChoice() == 'Rock') {
+        console.log('You Lose! Paper beats rock!')
+    }
+    else if (getComputerChoice() == 'Paper' && getHumanChoice() == 'Paper') {
+        console.log('Draw! Both is Paper!')
+    }
+    else if (getComputerChoice() == 'Scissors' && getHumanChoice() == 'Rock') {
+        console.log('You Won! Rock beats Scissors!')
+    }
+    else if (getComputerChoice() == 'Scissors' && getHumanChoice() == 'Paper') {
+        console.log('You Lose! Scissors beats Paper!')
+    }
+    else if (getComputerChoice() == 'Scissors' && getHumanChoice() == 'Scissors') {
+        console.log('Draw! Both is Scissors!')
+    }
+    
 }
 
 function getHumanChoice() {
     let humanChoice = prompt('Input number for: 1. Rock 2. Paper 3. Scissors');
-    
-
-    if (humanChoice == 1) {
-        console.log('Rock');
+    switch(humanChoice){
+        case 1:
+            return 'Rock'
+        case 2:
+            return 'Paper'
+        case 3:
+            return 'Scissors'
     }
-    else if (humanChoice == 2) {
-        console.log('Paper')
-    }
-    else if (humanChoice == 3) {
-        console.log('Scissors')
-    }
-    return humanChoice;
-    
 }
-    
-//ROCK PAPER SCISSORS AS STRING GENERATION  
+
 
 
 
