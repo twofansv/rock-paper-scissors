8e38a684f787b82971bb5cd47acefd3c4b12699d
twofansv <95734976+twofansv@users.noreply.github.com>
2025-08-03 19:25:05 +0800

    Changed the logic of versusChoice to an Object logic for optimization and reduce redundancy of If-Else statements.

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
 // MAKE A NUMBER GENERATOR FROM NUMBER 0 TO 3
 // BOX FOR NUMBER GENERATED
 // IF THE GENERATED NUMBER IS EQUALS TO 1 THEN IT MUST PLAY "ROCK" ELSE IF .. UPTO SCISSORS ..
-/// 
+//
+// RANDOM NUMBER GENERATOR
 
 
+const humanScore = 0;
+const computerScore = 0;
 
 
-
-
-
-
-
-
-//RANDOM NUMBER GENERATOR
 function getComputerChoice(){
     let numberChoice = Math.floor(Math.random() * 3);
 switch(numberChoice){
@@ -24,48 +20,59 @@ switch(numberChoice){
         return 'Scissors'
     }
 }
-   
 
 let instanceOfComputer = getComputerChoice();
 let instanceOfHuman = getHumanChoice();
 
 console.log('Computer chose ' + instanceOfComputer + ' and you chose ' + instanceOfHuman + ' so...');
 
-
-
 function versusChoice() {
 
-    let compareChoice;
+    const winMap = {
+        Rock: 'Scissors',
+        Paper: 'Rock',
+        Scissors: 'Paper'
+    }
 
-    if (instanceOfComputer == 'Rock' && instanceOfHuman == 'Paper') {
-        compareChoice = 'You won! Paper beats Rock!';
+    if (instanceOfComputer === instanceOfHuman){
+        return (`Draw! ${instanceOfComputer} and ${instanceOfHuman} = Tie!`);
     }
-    else if (instanceOfComputer == 'Rock' && instanceOfHuman == 'Scissors') {
-        compareChoice = 'You lose! Rock beats Scissors!';
+    else if (winMap[instanceOfHuman] === instanceOfComputer){
+        return (`You won! ${instanceOfHuman} beats ${instanceOfComputer}!`);
     }
-    else if (instanceOfComputer == 'Rock' && instanceOfHuman == 'Rock') {
-        compareChoice = 'Draw! Both are Rocks!';
+    else if (winMap[instanceOfComputer] === instanceOfHuman){
+        return (`You lose! ${instanceOfComputer} beats ${instanceOfHuman}!`);
     }
-    else if (instanceOfComputer == 'Paper' && instanceOfHuman == 'Scissors') {
-        compareChoice = 'You won! Scissors beats Paper!';
-    }
-    else if (instanceOfComputer == 'Paper' && instanceOfHuman == 'Rock') {
-        compareChoice = 'You lose! Paper beats Rock!';
-    }
-    else if (instanceOfComputer == 'Paper' && instanceOfHuman == 'Paper') {
-        compareChoice = 'Draw! Both are Paper!';
-    }
-    else if (instanceOfComputer == 'Scissors' && instanceOfHuman == 'Rock') {
-        compareChoice = 'You Won! Rock beats Scissors!';
-    }
-    else if (instanceOfComputer == 'Scissors' && instanceOfHuman == 'Paper') {
-        compareChoice = 'You lose! Scissors beats Paper!';
-    }
-    else if (instanceOfComputer == 'Scissors' && instanceOfHuman == 'Scissors') {
-        compareChoice = 'Draw! Both are Scissors!';
-    }
-    return compareChoice;
-    
+
+
+    // if (instanceOfComputer == 'Rock' && instanceOfHuman == 'Paper') {
+    //     compareChoice = 'You won! Paper beats Rock!';
+    // }
+    // else if (instanceOfComputer == 'Rock' && instanceOfHuman == 'Scissors') {
+    //     compareChoice = 'You lose! Rock beats Scissors!';
+    // }
+    // else if (instanceOfComputer == 'Rock' && instanceOfHuman == 'Rock') {
+    //     compareChoice = 'Draw! Both are Rocks!';
+    // }
+    // else if (instanceOfComputer == 'Paper' && instanceOfHuman == 'Scissors') {
+    //     compareChoice = 'You won! Scissors beats Paper!';
+    // }
+    // else if (instanceOfComputer == 'Paper' && instanceOfHuman == 'Rock') {
+    //     compareChoice = 'You lose! Paper beats Rock!';
+    // }
+    // else if (instanceOfComputer == 'Paper' && instanceOfHuman == 'Paper') {
+    //     compareChoice = 'Draw! Both are Paper!';
+    // }
+    // else if (instanceOfComputer == 'Scissors' && instanceOfHuman == 'Rock') {
+    //     compareChoice = 'You Won! Rock beats Scissors!';
+    // }
+    // else if (instanceOfComputer == 'Scissors' && instanceOfHuman == 'Paper') {
+    //     compareChoice = 'You lose! Scissors beats Paper!';
+    // }
+    // else if (instanceOfComputer == 'Scissors' && instanceOfHuman == 'Scissors') {
+    //     compareChoice = 'Draw! Both are Scissors!';
+    // }
+    // return compareChoice;
 }
 
 function getHumanChoice() {
@@ -81,29 +88,7 @@ function getHumanChoice() {
     else if (humanChoice == 'scissors'){
         return 'Scissors'
     }
-    
 }
 
-
-
-
-
 console.log(versusChoice());
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
