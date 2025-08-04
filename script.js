60fb68d89399f220db37049cb4918f9f2c4723ac
twofansv <95734976+twofansv@users.noreply.github.com>
2025-08-04 21:40:24 +0800

    Deleted comments related to keeping old code in versusChoice.

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ let instanceOfHuman = getHumanChoice();
 
 console.log('Computer chose ' + instanceOfComputer + ' and you chose ' + instanceOfHuman + ' so...');
 
-function versusChoice() {
+function versusChoice(instanceOfComputer, instanceOfHuman) {
 
     const winMap = {
         Rock: 'Scissors',
@@ -44,35 +44,6 @@ function versusChoice() {
         return (`You lose! ${instanceOfComputer} beats ${instanceOfHuman}!`);
     }
 
-
-    // if (instanceOfComputer == 'Rock' && instanceOfHuman == 'Paper') {
-    //     compareChoice = 'You won! Paper beats Rock!';
-    // }
-    // else if (instanceOfComputer == 'Rock' && instanceOfHuman == 'Scissors') {
-    //     compareChoice = 'You lose! Rock beats Scissors!';
-    // }
-    // else if (instanceOfComputer == 'Rock' && instanceOfHuman == 'Rock') {
-    //     compareChoice = 'Draw! Both are Rocks!';
-    // }
-    // else if (instanceOfComputer == 'Paper' && instanceOfHuman == 'Scissors') {
-    //     compareChoice = 'You won! Scissors beats Paper!';
-    // }
-    // else if (instanceOfComputer == 'Paper' && instanceOfHuman == 'Rock') {
-    //     compareChoice = 'You lose! Paper beats Rock!';
-    // }
-    // else if (instanceOfComputer == 'Paper' && instanceOfHuman == 'Paper') {
-    //     compareChoice = 'Draw! Both are Paper!';
-    // }
-    // else if (instanceOfComputer == 'Scissors' && instanceOfHuman == 'Rock') {
-    //     compareChoice = 'You Won! Rock beats Scissors!';
-    // }
-    // else if (instanceOfComputer == 'Scissors' && instanceOfHuman == 'Paper') {
-    //     compareChoice = 'You lose! Scissors beats Paper!';
-    // }
-    // else if (instanceOfComputer == 'Scissors' && instanceOfHuman == 'Scissors') {
-    //     compareChoice = 'Draw! Both are Scissors!';
-    // }
-    // return compareChoice;
 }
 
 function getHumanChoice() {
@@ -90,5 +61,5 @@ function getHumanChoice() {
     }
 }
 
-console.log(versusChoice());
+console.log(versusChoice(instanceOfComputer, instanceOfHuman));
 
